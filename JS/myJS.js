6bccd7beaf10f6fbc327eea5b363e9cdb9f28fa9
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-02-22 23:00:38 +0330

    Finishing the Style and Script
    Finishing the Style and Script

--- JS/myJS.js
+++ JS/myJS.js
@@ -1,7 +1,20 @@
 window.alert = (msg,timeout=null)=>{
     const alert  = document.createElement('div');
     const btnAlert =  document.createElement('button');
-    btnAlert.innerText = 'Ok'    ;
+    const Okey = "Ok";
+    btnAlert.innerHTML=`<span style ="display:flex;
+                                      justify-content:center;
+                                      align-items:center;
+                                      padding:10px;
+                                      width:10px;
+                                      color:red;
+                                      font-size:15px;">${Okey}</span>`;
+    btnAlert.setAttribute('style',`
+        background :rgb(226, 224, 224);
+        width:45px;
+        border-radius:5px;
+        `
+        );
     
     alert.classList.add('alert');
     alert.setAttribute('style',`
@@ -12,8 +25,10 @@ window.alert = (msg,timeout=null)=>{
         padding:15px;
         border: 1px solid #345642;
         border-radius:8px;
-        box-shadow: 3px 4px 5px #223322;
+        box-shadow: 6px 6px 12px #223322;
         display:flex;
+        justify-content:center;
+        align-items:center;
         flex-direction:column;
         
         `);
